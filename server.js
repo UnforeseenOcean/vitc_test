187c6cfb3797679061d1a0f5cbb88d957cc9a306
rpboyer15 <rpboyer15@gmail.com>
2018-08-26 13:38:33 -0400

    jrouter

--- server.js
+++ server.js
@@ -13,12 +13,30 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send('gotten');
-})
+});
 
 app.post('/battery', (req, res) => {
-	console.log(req.body.payload);
+	var lowChucks = [];
+	var payload = req.body.payload;
+	for (var i = 0; i < payload.length; i++) {
+		var logObj = getJSON(payload[i]);
+		var lowBattery = getBattery(logObj.msg);
+		var chuck = logObj.mfp.id;
+		var inPool = lowChucks.includes(chuck);
+		console.log(chuck+'\n'+(lowBattery && !inPool)+'\n'+(!lowBattery && inPool));
+		if (lowBattery && !inPool) {
+		  lowChucks.push(chuck);
+		  console.log(lowChucks);
+		} 
+		else if (!lowBattery && inPool) {
+		  console.log("High battery and in pool");
+		  var index = lowChucks.indexOf(chuck);
+		  lowChucks.splice(index, 1);
+		  console.log("end of high battery statement");
+		}
+	}
 	res.send('sent battery info');
-})
+});
 
 app.listen(port);
 
@@ -36,4 +54,24 @@ function normalizePort(val) {
   }
 
   return false;
+}
+
+function getJSON(log) {
+  for (var i = 0; i < log.length; i++) {
+    if (log.charAt(i) == '{') {
+      return JSON.parse(log.substring(i));
+    }
+  }
+}
+
+function getBattery(msg) {
+  var regex = /m\): (\d+)/;
+  var result = msg.match(regex);
+  regex = /\d+/;
+  var timeToEmpty = ((Number(result[0].match(regex)) + Number(result[1].match(regex))) / 2);
+  if (timeToEmpty < 900) {
+    return true;
+  } else {
+    return false;
+  }
 }
